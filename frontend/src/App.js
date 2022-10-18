0848d54e2691f1bb890edcbbd4244a64682c4598
Ashimkarrki <ashimkarki987@gmail.com>
2022-10-18 11:06:30 +0545

    set up done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./Style/main.scss";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
 function App() {
+  let location = useLocation();
+  console.log(location.pathname);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {location.pathname === "/signup" || location.pathname === "/login" ? (
+        ""
+      ) : (
+        <Navbar />
+      )}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
