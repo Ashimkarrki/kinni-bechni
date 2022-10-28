4270ef433cdb2a141c0b6628f189f2bbe0e98583
patali <patali@kali>
2022-10-28 16:49:33 +0545

    docker

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./Style/main.scss";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+function App() {
+  let location = useLocation();
+  console.log(location.pathname);
+  return (
+    <div className="App">
+      {location.pathname === "/signup" || location.pathname === "/login" ? (
+        ""
+      ) : (
+        <Navbar />
+      )}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ joinme/app/Http/Controllers/authentication.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\auth;
+class authentication extends Controller
+
+
+{
+    public function signup(Request $request){
+        // $message = ['email.required'=>"Something is wrong",'name.required'=>"name is wrong",'password.required'=>"password is wrong",'confirm_password.required'=>"password is wrong"];
+        // $this->validate([
+        //     'email'=>'required | max:20',
+        //     'name'=>'required | max:20',
+        //     'password'=>'required|min:8|max:13',
+        //     'confirm_password'=>'required'
+        // ], $message);
+
+            if ($request->password === $request->confirm_password){
+                $usercontroller_inst = new auth;
+                $usercontroller_inst->email = $request->email;
+                $usercontroller_inst->name = $request->name;
+                $usercontroller_inst ->password = $request-> password;
+                $usercontroller_inst ->college = $request ->college;
+                $usercontroller_inst->save();
+                return response()->json([
+                    'status' => '200',
+                    'message'=>'Registered successfully',
+                ]);}
+            else{
+                return "Password and Confirm Password field should be same";
+            }
+        }
+    
+    
+
+    
+    public function login(Request $request){
+        $user = auth::where('email', $request->email)->first();
+        if ($request->password===$user->password){
+            return Response()->json([
+                "Status"=>200,
+                "Message"=>"User Logged in successfully"
+            ]);
+    }
+        else{
+            return response()->json([
+                "message"=>"Unable to log in"
+            ]);
+                
+            }
+        }
+    public function logined(Request $request){
+    }
+
+    public function logout(Request $request){
+        return redirect("login");
+    }
+};
+
