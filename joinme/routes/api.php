3322f5367fcd809c958b0311dd9529e2cda7421b
patali <patali@kali>
2022-11-09 09:19:50 +0545

    connection error

--- joinme/routes/api.php
+++ joinme/routes/api.php
@@ -24,4 +24,7 @@ Route::post('/login', [authentication::class, "login"]);
 Route::get('/logout', [authentication::class, "logout"]);
 Route::get('login', [authentication::class, "logined"]);
 Route::get("/",[ProductController::class,"homepage"]);
+Route::get("/storage/app/public", function(){
+    return response()->file('/storage/app/public/test.jpg');
+});
 Route::post('/addproduct', [ProductController::class, 'addproduct']);
