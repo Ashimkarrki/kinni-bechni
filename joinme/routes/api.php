d0c239c898773a6c26cd6f99e10f2f94c2e23341
NIraj Neupane <neupaneniraj08@gmail.com>
2022-11-13 11:05:03 +0545

    id2 issue solved

--- joinme/routes/api.php
+++ joinme/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authentication;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Storage;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,8 +26,8 @@ Route::post('/login', [authentication::class, "login"]);
 Route::get('/logout', [authentication::class, "logout"]);
 Route::get('login', [authentication::class, "logined"]);
 Route::get("/",[ProductController::class,"homepage"]);
-Route::get("/storage/app/public", function(){
-    return response()->file('/storage/app/public/test.jpg');
+Route::get("/storage/{filename}", function($filename){
+    echo asset('storage/'.$filename);
 });
 Route::post('/addproduct', [ProductController::class, 'addproduct']);
 Route::post('/getproduct', [ProductController::class, 'returnproduct']);
